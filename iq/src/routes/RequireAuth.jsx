2889155dfdd27ccb045d57990e01d912aa119108
quangnvph26060 <sgovietnam.dev@gmail.com>
2025-05-06 10:55:55 +0700

    update code

--- iq/src/routes/RequireAuth.jsx
+++ iq/src/routes/RequireAuth.jsx
@@ -4,9 +4,9 @@ import { useAuth } from '../contexts/AuthContext';
 const RequireAuth = ({ children }) => {
   const { token, isAuthReady } = useAuth();
 
-  // ⏳ Chờ khởi tạo xong mới kiểm tra token
+
   if (!isAuthReady) {
-    return null; // hoặc loading spinner nếu muốn
+    return null;
   }
 
   if (!token) {
